5fe7103629e52b8242abe6c47b9508ce1ca2a0b1
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-16 17:25:39 -0500

    commit tree 431f40868bf4b5c9753e0338852d70146b5d48b7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+number = 10
 
-def countdown
+def countdown (number)
+  while number == 1
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+    "HAPPY NEW YEAR!"
 end
